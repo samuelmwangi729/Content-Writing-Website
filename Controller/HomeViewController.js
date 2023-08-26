3437009e33c036ab6f05942c2362e4b1e7242e23
Samuel Mwangi <42680795+samuelmwangi729@users.noreply.github.com>
2023-08-26 15:59:51 +0300

    Frontend Links created and Tested

--- Controller/HomeViewController.js
+++ Controller/HomeViewController.js
@@ -6,5 +6,32 @@ const Index = (req,res)=>{
     title='HomePage'
     res.render('Index.ejs',{title:title})
 }
-
-module.exports = {Index}
+const About  = (req,res)=>{
+    title="About Us"
+    res.render('pages/About.ejs',{title:title})
+}
+const Services = (req,res)=>{
+    title="Our Services"
+    res.render('pages/Services.ejs',{title:title})
+}
+const Pricing = (req,res)=>{
+    title='Our Pricing Policy'
+    res.render('pages/Pricing.ejs',{title:title})
+}
+const Portfolio = (req,res)=>{
+    title='Our Portfolio'
+    res.render('pages/Portfolio.ejs',{title:title})
+}
+const FAQ = (req,res)=>{
+    title='Frequently Asked Questions'
+    res.render('pages/FAQ.ejs',{title:title})
+}
+const Blog = (req,res)=>{
+    title="Our Blog"
+    res.render('pages/Blog.ejs',{title:title})
+}
+const Contact = (req,res)=>{
+    title='Contact Us'
+    res.render('pages/Contact.ejs',{title:title})
+}
+module.exports = {Index,About,Services,Pricing,Portfolio,FAQ,Blog,Contact}
--- Routes/Home.js
+++ Routes/Home.js
@@ -1,8 +1,16 @@
 const express = require('express')
-const {Index} = require('../Controller/HomeViewController')
+const {Index,
+    About,Services,Pricing,Portfolio,FAQ,Blog,Contact} = require('../Controller/HomeViewController')
 const homeRoute = express.Router()
 
 homeRoute.get("/",Index)
+.get('/About',About)
+.get('/Services',Services)
+.get('/Pricing',Pricing)
+.get('/Portfolio',Portfolio)
+.get('/Blog',Blog)
+.get('/FAQ',FAQ)
+.get('/Contact',Contact)
 
 
 module.exports = homeRoute
