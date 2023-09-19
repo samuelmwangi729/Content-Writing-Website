2da111c5175d71f7411b6992e9ba1adc55168655
S M <samuelmwangi729@gmail.com>
2023-09-19 12:18:32 +0300

    final

---
+++ Models/Bids.js
@@ -0,0 +1,38 @@
+const User = require('./Users')
+const Order = require('./Orders')
+const {Schema,model} = require('mongoose')
+
+const BidSchema = new Schema({
+    //then the types 
+    ProjectID:{
+        type:Schema.Types.ObjectId,
+        ref:Order,
+        required:[true,'The Project Id is required']
+    },
+    BidTitle:{
+        type:String,
+        required:[true,'The title is required'],
+    },
+    BidDescription:{
+        type:String,
+        required:[true,'The description is required']
+    },
+    BidType:{
+        type:String,
+        enum:['Sealed','Highlighted','Sponsored','Basic'],
+        default:'Basic'
+    },
+    BidStatus:{
+        type:String,
+        enum:['Dropped','Withdrawn','Accepted','Pending'],
+        default:'Pending'
+    },
+    Freelancer:{
+        type:Object.Types.ObjectId,
+        ref:User,
+        required:[true,'The user is required']
+    }
+},{timestamps:true})
+const Bids = model('Bids',BidSchema)
+//export the model 
+module.exports = Bids
